aa621b0e167323533e0d9fcae10627306d1745d7
frankline20 <ugwu.nnaemekan@gmail.com>
2014-11-24 12:49:31 +0100

    working2 without object literal

--- js/main.js
+++ js/main.js
@@ -1,77 +1,57 @@
-var button = document.getElementById('Action'),
-output =  document.getElementById('hotter_colder'),
-guess_input = document.getElementById('guess'),
-counter = document.getElementById('counter'),
-man_image = document.getElementById('man'),
-new_button = document.getElementById('left'),
-
-guess_count = 0,
-valid_guess = 0,
-hidden_number = Math.floor(100*Math.random()),
-computer_present = 0,
-computer_previous = 101;
-
-var init = function() {
-	button = document.getElementById('Action');
-	output =  document.getElementById('hotter_colder');
-	guess_input = document.getElementById('guess');
-	counter = document.getElementById('counter');
-	man_image = document.getElementById('man');
-	new_button = document.getElementById('left');
-
-	guess_count = 0;
-	valid_guess = 0;
-	hidden_number = Math.floor(100*Math.random());
-	computer_present = 0;
-	computer_previous = 101;
-}
-	
 new_game =  function(){
-	guess_count = valid_guess = computer_present = 0;
-	man_image.style.marginLeft = 0;
+	
+	valid_guess = 0,
+	hidden_number = Math.floor(100*Math.random()),
+	computer_present = 0,
 	computer_previous = 101;
-	hidden_number = Math.floor(100*Math.random());
-	button.innerHTML = "Guess";
-	counter.innerHTML = "0";
-	output.innerHTML = "Waiting for your guess!";
-	guess_input.value = ""
+
+	document.getElementById('man').style.marginLeft = 0;
+	document.getElementById('Action').innerHTML = "Guess";
+	document.getElementById('counter').innerHTML = "0";
+	document.getElementById('hotter_colder').innerHTML = "Waiting for your guess!";
+	document.getElementById('guess').value = ""
+	document.getElementById('man').src = "img/neither.jpg";
+}
+
+set_image = function(img_url){
+	document.getElementById('man').style.marginLeft = (100-computer_present) * 2.08 + "px";
+	document.getElementById('man').src = img_url;
 }
 
 validate_guess = function(user_guess){
 	if((typeof user_guess === typeof 1) && ( user_guess <=100 && user_guess >= 0) ){
-		guess_input.value = "";
+		document.getElementById('guess').value = "";
 		return true;
 	}
 	return false;
 }
 
 guess = function(){
-	if(button.innerHTML == "New Game")
+	if(document.getElementById('Action').innerHTML == "New Game")
 		new_game();
 	else{
-		var user_guess = parseInt(guess_input.value);
+		var user_guess = parseInt(document.getElementById('guess').value);
 		if (validate_guess(user_guess)){
 			valid_guess = user_guess;
 			if(compare_guess()){
-				output.innerHTML = "You guessed it right!";
-				button.innerHTML = "New Game";
+				document.getElementById('hotter_colder').innerHTML = "You guessed it right!";
+				document.getElementById('Action').innerHTML = "New Game";
 			}
 			else{
-				output.innerHTML = compare_previous();
+				document.getElementById('hotter_colder').innerHTML = compare_previous();
 				computer_previous = computer_present;
-				counter.innerHTML = (parseInt(counter.innerHTML)+1).toString();
+				document.getElementById('counter').innerHTML = (parseInt(document.getElementById('counter').innerHTML)+1).toString();
 			}
 		} else 
-		output.innerHTML ="invalid input";
+		document.getElementById('hotter_colder').innerHTML ="invalid input";
 	}
-},
+}
 
 compare_guess = function(){
 	computer_present = Math.abs(hidden_number-valid_guess);
 	
 	if(valid_guess === hidden_number) {
-		man_image.style.marginLeft = (100-computer_present) * 2.08 + "px";
-		man_image.src = "img/neither.jpg";
+		set_image("img/neither.jpg");
 		return true;
 	}
 	return false;
@@ -81,20 +61,18 @@ compare_previous = function(){
 	computer_present = Math.abs(hidden_number-valid_guess);
 
 	if( computer_present === computer_previous ){
-		man_image.src = "img/neither.jpg";
+		set_image("img/neither.jpg");
 		return "Neither cold nor hot";
 
 	} else if (computer_present > computer_previous){
-		man_image.src = "img/walking_back.jpg";
-		man_image.style.marginLeft = (100-computer_present) * 2.08 + "px";
+		set_image("img/walking_back.jpg");
 		return "you are getting colder";
 	}
 
-	man_image.src = "img/walking_front.jpg";
-	man_image.style.marginLeft = (100-computer_present) * 2.08 + "px";
+	set_image("img/walking_front.jpg");
 	return "you are getting hotter";
 }
 
-
-button.addEventListener("click", guess);
-new_button.addEventListener("click", new_game);
+new_game();
+document.getElementById('Action').addEventListener("click", guess);
+document.getElementById('left').addEventListener("click", new_game);
